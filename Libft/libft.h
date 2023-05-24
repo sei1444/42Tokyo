9bb67ebaebf939a338cfe06d08d1491b3c58cfb9
sei1444 <sei21tas1422@gmail.com>
2023-05-24 18:18:36 +0900

    2023/05/24/18:18

---
+++ Libft/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seono <seono@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/20 19:08:31 by seono             #+#    #+#             */
+/*   Updated: 2023/05/20 19:28:59 by seono            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+int	ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+
+#endif
